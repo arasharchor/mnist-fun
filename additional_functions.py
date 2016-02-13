d975185e18d925e1ba0e2ef53f00e1caeaa22a84
Jesse Lieman-Sifry <jliemansifry@wesleyan.edu>
2016-02-12 22:31:28 -0500

    First commit

---
+++ additional_functions.py
@@ -0,0 +1,75 @@
+import numpy as np
+from keras.models import model_from_json
+
+
+def load_model(path_to_model):
+    ''' 
+    INPUT:  (1) String: The path to the saved model architecture and weights, 
+                not including .json or .h5 at the end
+    OUTPUT: (2) Trained and compiled Keras model 
+    '''
+    json_file_name = '{}.json'.format(path_to_model)
+    weights_file_name = '{}.h5'.format(path_to_model)
+    model = model_from_json(open(json_file_name).read())
+    model.load_weights(weights_file_name)
+    model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+    return model
+
+
+def predict_classwise_top_n_acc(model, X_test, y_test, n=1):
+    ''' 
+    INPUT:  (1) Trained and compiled Keras model
+            (2) 4D numpy array: all the test set images
+            (3) 1D numpy array: the corresponding test set labels
+    OUTPUT: (1) Dictionary: the classes as keys, with corresponding 
+                accuracies as values
+
+    This function is able to calculate the top-n accuracy on a classwise basis.
+    '''
+    unique_classes = np.unique(y_test)
+    y_test = y_test.reshape((y_test.shape[0], 1))
+    probas = model.predict_proba(X_test, batch_size=32)
+    top_n_guesses = np.fliplr(np.argsort(probas, axis=1))[:, :n]
+    classwise_acc_dict = {}
+    for unique_class in unique_classes:
+        unique_class_locs = np.where(y_test == unique_class)[0]
+        top_n_guesses_for_this_class = top_n_guesses[unique_class_locs]
+        in_top_n = [1 if y_test[row_idx] in row
+                    else 0
+                    for row_idx, row 
+                    in zip(unique_class_locs, top_n_guesses_for_this_class)]
+        classwise_acc_dict[unique_class] = (np.sum(in_top_n) / 
+                                            float(len(unique_class_locs)))
+    return classwise_acc_dict
+    
+
+def add_gaussian_noise(X_train, mean, stddev):
+    ''' 
+    INPUT:  (1) 4D numpy array: all raw training image data, of shape 
+                (#imgs, #chan, #rows, #cols)
+    OUTPUT: (1) 4D numpy array: noisy training data
+    '''
+    n_imgs = X_train.shape[0]
+    n_chan = X_train.shape[1]
+    n_rows = X_train.shape[2]
+    n_cols = X_train.shape[3]
+    noise = np.random.normal(mean, stddev, (n_imgs, n_chan, n_rows, n_cols))
+    noisy_X = X_train + noise
+    clipped_noisy_X = np.clip(noisy_X, 0., 255.)
+    return clipped_noisy_X
+
+
+def add_label_noise(y_train, percent_to_randomize):
+    ''' 
+    INPUT:  (1) 1D numpy array: y training data
+            (2) float: the fraction of data to randomize the labels for
+    OUTPUT: (1) 1D numpy array: noisy y training data
+    '''
+    n_labels = y_train.shape[0]
+    n_to_randomize = n_labels * percent_to_randomize
+    n_random_labels = np.random.randint(low=0, high=10, size=n_to_randomize)
+    ordered_ind = np.arange(n_labels)
+    permuted_ind = np.random.permutation(ordered_ind)
+    random_ind = permuted_ind[:n_to_randomize]
+    y_train[random_ind] = n_random_labels
+    return y_train
---
+++ keras_model.py
@@ -0,0 +1,133 @@
+import numpy as np
+np.random.seed(1234)  # for reproducibility
+import time
+import os
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+from keras.datasets import mnist
+
+
+def set_basic_model_param():
+    ''' 
+    INPUT:  None
+    OUTPUT: (1) Dictionary of important values for formatting data and 
+                compiling model. 
+
+    For lightweight tuning of the model (ie. no change in overall structure) 
+    it's easiest to keep all model parameters in one place.
+    '''
+    model_param = {'n_rows': 28, 
+                   'n_cols': 28,
+                   'n_chan': 1,
+                   'n_classes': 10,
+                   'n_epoch': 4,
+                   'batch_size': 32,
+                   'pool_size': 2,
+                   'conv_size': 3,
+                   'n_conv_nodes': 32,
+                   'n_dense_nodes': 128,
+                   'primary_dropout': 0.25,
+                   'secondary_dropout': 0.5,
+                   'model_build': 'v.0.1'}
+    return model_param
+
+
+def load_and_format_mnist_data(model_param, categorical_y=False):
+    ''' 
+    INPUT:  (1) Dictionary: values important for formatting data appropriately
+    OUTPUT: (1) 3D numpy array: the X training data, of shape (#train_images,
+                #rows, #columns); for MNIST this is (60000, 28, 28)
+            (2) 1D numpy array: the training labels, y, of shape (60000,)
+            (3) 3D numpy array: the X test data, of shape (#test_images, 
+                #rows, #columns); for MNIST this is (10000, 28, 28)
+            (4) 1D numpy array: the test labels, of shape (10000,)
+
+    This function loads the data and labels, reshapes the data to be in the 
+    4D tensor shape that Keras requires (#images, #color_channels, 
+    #rows, #cols) for training, and returns it. The method load_data() 
+    returns the MNIST data, shuffled and split between train and test set.
+    '''
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    num_train_images, num_test_images = X_train.shape[0], X_test.shape[0]
+    X_train = X_train.reshape(num_train_images, 
+                              model_param['n_chan'], 
+                              model_param['n_rows'],
+                              model_param['n_cols'])
+    X_test = X_test.reshape(num_test_images, 
+                            model_param['n_chan'],
+                            model_param['n_rows'],
+                            model_param['n_cols'])
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    X_train /= 255.
+    X_test /= 255.
+
+    if categorical_y:
+	# convert class vectors to binary class matrices
+        y_train = np_utils.to_categorical(y_train, model_param['n_classes'])
+        y_test = np_utils.to_categorical(y_test, model_param['n_classes'])
+    return X_train, y_train, X_test, y_test
+
+
+def compile_model(model_param):
+    model = Sequential()
+    model_param_to_add = [Convolution2D(model_param['n_conv_nodes'], 
+                                        model_param['conv_size'],
+                                        model_param['conv_size'],
+                                        border_mode='valid',
+                                        input_shape=(model_param['n_chan'],
+                                                     model_param['n_rows'],
+                                                     model_param['n_cols'])),
+                          Activation('relu'),
+                          Convolution2D(model_param['n_conv_nodes'], 
+                                        model_param['conv_size'],
+                                        model_param['conv_size']),
+                          Activation('relu'),
+                          MaxPooling2D(pool_size=(model_param['pool_size'],
+                                                  model_param['pool_size'])),
+                          Dropout(model_param['primary_dropout']),
+                          Flatten(),
+                          Dense(model_param['n_dense_nodes']),
+                          Activation('relu'),
+                          Dropout(model_param['secondary_dropout']),
+                          Dense(model_param['n_classes']),
+                          Activation('softmax')]
+
+    for process in model_param_to_add:
+        model.add(process)
+    model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+    return model
+
+
+def fit_and_save_model(model, model_param, X_train, y_train, X_test, y_test):
+    start = time.clock()
+    model.fit(X_train, y_train, batch_size=model_param['batch_size'],
+              nb_epoch=model_param['n_epoch'],
+              show_accuracy=True, verbose=1,
+              validation_data=(X_test, y_test))
+    stop = time.clock()
+    total_run_time = (stop - start) / 60.
+    score = model.evaluate(X_test, y_test, show_accuracy=True, verbose=0)
+    print 'Test score: {}'.format(score[0])
+    print 'Test accuracy: {}'.format(score[1])
+    print 'Total run time: {}'.format(total_run_time)
+
+    model_name = 'KerasBaseModel_{}'.format(model_param['model_build'])
+    path_to_save_model = 'models/{}'.format(model_name)
+    json_file_name = '{}.json'.format(path_to_save_model)
+    weights_file_name = '{}.h5'.format(path_to_save_model)
+    if os.path.isfile(json_file_name) or os.path.isfile(weights_file_name):
+        json_file_name = '{}_copy.json'.format(path_to_save_model)
+        weights_file_name = '{}_copy.h5'.format(path_to_save_model)
+        print 'Please rename the model next time to avoid conflicts!'
+    json_string = model.to_json()
+    open(json_file_name, 'w').write(json_string)
+    model.save_weights(weights_file_name)
+
+if __name__ == '__main__':
+    model_param = set_basic_model_param()
+    X_train, y_train, X_test, y_test = load_and_format_mnist_data(model_param)
+    model = compile_model(model_param)
+    fit_and_save_model(model, model_param, X_train, y_train, X_test, y_test)
